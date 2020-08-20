34143ef0831c4e11467c508f6c0b5eed190f96a9
ezezhub <69975325+ezezhub@users.noreply.github.com>
2020-08-20 16:48:25 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y stow
 
 # install zsh
 sudo apt-get install -y zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #stow all the dotfiles
 files="zsh"
@@ -14,7 +15,5 @@ for file in ${files}; do
   echo "Stow $file to home directory"
   stow $file
 done
-
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Completed"
