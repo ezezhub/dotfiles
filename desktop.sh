f96672e94c2f86ad7b827541f1c5d1b527396e2b
ezezhub <69975325+ezezhub@users.noreply.github.com>
2020-08-21 10:03:43 +0800

    Delete desktop.sh

--- desktop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# install desktop environment
-DIR=$HOME
-gsettings set org.gnome.desktop.background picture-uri "file://$HOME/dotfiles/941788.jpg"
-
-echo "Wallpaper setting completed"
