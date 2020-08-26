fcbe2bdda976fcda06234fe66855c8beb28b2a61
ezezhub <69975325+ezezhub@users.noreply.github.com>
2020-08-26 12:28:00 +0800

    Create intellj.sh

---
+++ installscript/intellj.sh
@@ -0,0 +1,6 @@
+# install IntelliJ
+VERSION="2020.2"
+wget -O /opt/intellij.tar.gz http://download.jetbrains.com/idea/ideaIC-$VERSION.tar.gz
+tar xvzf /opt/intellij.tar.gz
+sudo mv /opt/idea-IC-202.6397.94 /opt/idea
+./opt/idea/bin/idea.sh
