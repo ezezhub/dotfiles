b14246432f741d1d2dd0b6a436960d608b658724
ezezhub <69975325+ezezhub@users.noreply.github.com>
2020-08-26 12:28:13 +0800

    Update java.sh

--- installscript/java.sh
+++ installscript/java.sh
@@ -4,10 +4,3 @@
 sudo apt-get update
 sudo apt-get install -y default-jdk
 java -version
-
-# install IntelliJ
-VERSION="2020.2"
-wget -O /opt/intellij.tar.gz http://download.jetbrains.com/idea/ideaIC-$VERSION.tar.gz
-tar xvzf /opt/intellij.tar.gz
-sudo mv /opt/idea-IC-202.6397.94 /opt/idea
-./opt/idea/bin/idea.sh
