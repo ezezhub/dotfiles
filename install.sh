a7f53f3a148dbc9ad32799fa3cc2b2e5b27ad86f
ezezhub <69975325+ezezhub@users.noreply.github.com>
2020-08-26 10:50:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,11 @@ sudo apt-get install -y vim
 
 # install zsh
 sudo apt-get install -y zsh
+
+# change the default shell to zsh
 chsh -s /usr/bin/zsh
+
+# download oh-my-zsh and powerlevel10k
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
