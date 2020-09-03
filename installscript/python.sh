3632cc2f0d002fd2bd00975372fb7eb937cce335
ezez <ezez85@gmail.com>
2020-09-03 14:38:56 +0800

    add robot framework

--- installscript/python.sh
+++ installscript/python.sh
@@ -3,3 +3,9 @@
 # install python3
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
+
+packages=(
+    robotframework-seleniumlibrary
+)
+
+pip3 install --upgrade "${packages[@]}"
